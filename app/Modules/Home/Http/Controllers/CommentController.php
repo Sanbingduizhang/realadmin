2b704fed61c2209397b4b55a95547c6a32a481df
冯鑫龙 <18862005869@163.com>
2018-11-30 18:07:49 +0800

    11

--- app/Modules/Home/Http/Controllers/CommentController.php
+++ app/Modules/Home/Http/Controllers/CommentController.php
@@ -4,6 +4,7 @@ namespace App\Modules\Home\Http\Controllers;
 
 use App\Modules\Base\Http\Controllers\ApiBaseController;
 use App\Modules\Base\Models\Article;
+use App\Modules\Base\Models\ArticleComment;
 use App\Modules\Base\Repositories\ArticleCommentRepository;
 use App\Modules\Base\Repositories\ArticleRepository;
 use App\Modules\Base\Repositories\CategoryRepository;
@@ -57,8 +58,40 @@ class CommentController extends ApiBaseController
         return response_failed('评论失败');
     }
 
+    /**
+     * 首页评论删除，当前仅支持单个删除
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function arcomdel(Request $request)
     {
-        dd('comdel');
+        //获取数据
+        $id = $request->get('arcid','');
+        if (empty($id)) {
+            return response_failed('请输入相关参数');
+        }
+        //获取当前用户信息
+        $users = getUser($request);
+        //查找文章是否存在
+        $findArcRes = $this->arComRepository
+            ->where([
+                'id' => $id,
+                'is_del' => ArticleComment::IS_DEL_OFF,
+                'userid' => $users['id'],
+            ])->first();
+        if (empty($findArcRes)) {
+            return response_failed('数据有误');
+        }
+        //执行删除操作，更新字段
+        $delRes = $findArcRes->update(['is_del' => ArticleComment::IS_DEL_ON]);
+        if ($delRes) {
+            return response_success(['message' => '删除成功']);
+        }
+        return response_failed('删除失败');
+    }
+
+    public function likego()
+    {
+
     }
 }
