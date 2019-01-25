ff98a887cd78eadc915f2b730ee393d26c6bbfc5
冯鑫龙 <18862005869@163.com>
2019-01-25 12:00:46 +0800

    22

--- app/Modules/Wechat/Http/Controllers/WxController.php
+++ app/Modules/Wechat/Http/Controllers/WxController.php
@@ -25,12 +25,35 @@ class WxController extends Controller
     {
 
         $this->app->server->push(function ($message) {
-            if ($message->MsgType == 'event') {
-                if ($message->Event == 'subscribe') {
-                    return "欢迎关注易录播公众号";
-                } elseif ($message->Event == 'unsubscribe') {
-                    return "已经取消关注号";
-                }
+            Log::info($message);
+            switch ($message['MsgType']) {
+                case 'event':
+                    return '收到事件消息';
+                    break;
+                case 'text':
+                    return '收到文字消息';
+                    break;
+                case 'image':
+                    return '收到图片消息';
+                    break;
+                case 'voice':
+                    return '收到语音消息';
+                    break;
+                case 'video':
+                    return '收到视频消息';
+                    break;
+                case 'location':
+                    return '收到坐标消息';
+                    break;
+                case 'link':
+                    return '收到链接消息';
+                    break;
+                case 'file':
+                    return '收到文件消息';
+                // ... 其它消息
+                default:
+                    return '收到其它消息';
+                    break;
             }
         });
 
