fc1136dd5795425b5951993d7deb8600495111a5
冯鑫龙 <18862005869@163.com>
2019-01-28 11:33:23 +0800

    22

--- app/Modules/Wechat/Http/Controllers/WxController.php
+++ app/Modules/Wechat/Http/Controllers/WxController.php
@@ -79,6 +79,7 @@ class WxController extends Controller
     {
 //        $user = $this->app->oauth->user();
         Log::info('sessssss___' . session('wechat.oauth_user.default'));
+        dd(33333333);
         return header('location:http://148.70.67.47/shouquan.html');
 //        Log::info('bind __ ');
 //        return $this->app->oauth->scopes(['snsapi_userinfo'])->setRequest($request)->redirect();
