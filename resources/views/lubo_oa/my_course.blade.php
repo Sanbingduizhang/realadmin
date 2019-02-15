88c95bdceb8ffc7a3efa1bca06990d1a04e3350a
冯鑫龙 <18862005869@163.com>
2019-02-15 11:28:34 +0800

    121

--- resources/views/lubo_oa/my_course.blade.php
+++ resources/views/lubo_oa/my_course.blade.php
@@ -11,9 +11,9 @@
     <meta name="apple-mobile-web-app-status-bar-style" content="black">
     <meta name="format-detection" content="telephone=no">
     <!-- 插件 -->
-    <link rel="stylesheet" href="./css/style.css">
-    <!-- 本页样式文件 -->
-    <link rel="stylesheet" href="./css/index.css">
+    <link rel="stylesheet" href="{{ URL::asset('css/style.css') }}">
+    <!-- 项目页样式文件 -->
+    <link rel="stylesheet" href="{{ URL::asset('css/index.css') }}">
 </head>
 
 <body>
@@ -29,20 +29,20 @@
     <div class="course_list_wrap">
       <script type="text/html" id="tpl_course_list">
         <ul class="course_list">
-          {{each data.subject v i}}
+          [[each data.subject v i]]
             <li class="course_item">
-              {{if v.notify > 0 }}
-              <p class="notice text_ellipsis"> {{v.notify}}</p>
-              {{/if}}
+              [[if v.notify > 0 ]]
+              <p class="notice text_ellipsis"> [[v.notify]]</p>
+              [[/if]]
 
-              <img class="icon_cover" src="{{v.path}}" alt="{{v.name}}" data-subid="{{v.id}}">
+              <img class="icon_cover" src="[[v.path]]" alt="[[v.name]]" data-subid="[[v.id]]">
 
-              <!-- <a href="./video_list.html?subid={{v.id}}">
-                <img class="icon_cover" src="{{v.path}}" alt="{{v.name}}">
+              <!-- <a href="./video_list.html?subid=[[v.id]]">
+                <img class="icon_cover" src="[[v.path]]" alt="[[v.name]]">
               </a> -->
-              <p class="text_ellipsis">{{v.name}}</p>
+              <p class="text_ellipsis">[[v.name]]</p>
             </li>
-          {{/each}}
+          [[/each]]
        </ul>
       </script>
     </div>
@@ -60,37 +60,44 @@
     <div class="video_list_wrap">
       
       <script type="text/html" id="tpl_video_list">
-        {{if data.data && data.data.length > 0 }}
+        [[if data.data && data.data.length > 0 ]]
           <ul class="video_list" id="video_list_content">
-            {{each data.data v i}}
-              <li class="item" data-vid="{{v.video_id}}">
-                <video id="my_video_{{v.video_id}}" class="video_content" controls poster="{{v.img_path}}" videoid="{{v.video_id}}" onplay="begin_playing({{v.video_id}})">
-                  <source src="{{v.path}}" type="video/mp4">
+            [[each data.data v i]]
+              <li class="item" data-vid="[[v.video_id]]">
+                <video id="my_video_[[v.video_id]]" class="video_content" controls poster="[[v.img_path]]" videoid="[[v.video_id]]" onplay="begin_playing([[v.video_id]])">
+                  <source src="[[v.path]]" type="video/mp4">
                 </video>
                 <div class="detail">
-                  <p class="title text_ellipsis">{{v.video_title}}</p>
-                  <p class="date">{{v.created_at}}</p>
+                  <p class="title text_ellipsis">[[v.video_title]]</p>
+                  <p class="date">[[v.created_at]]</p>
                 </div>
               </li>
-            {{/each}}
+            [[/each]]
           </ul>
-        {{else}}
+        [[else]]
           <div class="no_data_text">暂无更多数据</div>
-        {{/if}}
+        [[/if]]
       </script>
     </div>
   </div>
   <!-- 视频列表 END -->
-
+  <input type="text" value="{{ $yj_wx_token }}" id="yj_token_wx" style="display: none;">
+  <input type="text" value="{{ $yj_wx_name }}" id="yj_token_user_name" style="display: none;">
 
   <footer class="footer">
-    <a href="./bind_account.html">陈晓梅</a><a class="my_video_course" href="./my_course.html">我的微课</a>
+    <a href="{{ URL::route('wx.bind-account',['openid' => $openid]) }}">陈晓梅</a><a class="my_video_course" href="{{ URL::route('wx.my-course',['openid' => $openid]) }}">我的微课</a>
   </footer>
-
-<script src="../js//zeptojs1.2.comzepto.min.js"></script>
-<script src="../js/template-web.js"></script>
-<script src="../js/dropload.js"></script>
-<script src="../js/index.js"></script>
+  
+<script src="{{ URL::asset('js/zeptojs1.2.comzepto.min.js') }}"></script>
+<script src="{{ URL::asset('js/template-web.js') }}"></script>
+<script>
+  //重写js模板语法
+  var rule = template.defaults.rules[1];
+  rule.test = new RegExp(rule.test.source.replace('\{\{', '\\[\\[').replace('\}\}', '\\]\\]'));
+</script>
+  
+<script src="{{ URL::asset('js/dropload.js') }}"></script>
+<script src="{{ URL::asset('js/index.js') }}"></script>
 <script>
   var getUrlParams = getSearchObj();
   $('#hasplay').attr('data-hasdata', '1');
